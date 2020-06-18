7f737975559ef663ec32417841f4627723dda413
hoashi <akane.hoashi@outlook.jp>
2020-06-18 13:05:46 +0900

    追加：商品用Dto

---
+++ SampleShopping/src/dto/GoodsDto.java
@@ -0,0 +1,77 @@
+package dto;
+
+public class GoodsDto {
+
+	private int id;
+	private String goodsName;
+	private int price;
+	private int stock;
+	private String description;
+	private String imageDir;
+	private int salesQuantity;
+	private boolean sale;
+	private int categoryId;
+	private int brandId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImageDir() {
+		return imageDir;
+	}
+	public void setImageDir(String imageDir) {
+		this.imageDir = imageDir;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public int getBrandId() {
+		return brandId;
+	}
+	public void setBrandId(int brandId) {
+		this.brandId = brandId;
+	}
+	public int getSalesQuantity() {
+		return salesQuantity;
+	}
+	public void setSalesQuantity(int salesQuantity) {
+		this.salesQuantity = salesQuantity;
+	}
+	public boolean isSale() {
+		return sale;
+	}
+	public void setSale(boolean sale) {
+		this.sale = sale;
+	}
+	
+}
