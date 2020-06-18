a887f680f2b3cf5ecda05b6e96c376e99be3c260
hoashi <akane.hoashi@outlook.jp>
2020-06-18 13:05:34 +0900

    追加：商品用Dao

---
+++ SampleShopping/src/dao/GoodsDao.java
@@ -0,0 +1,158 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.GoodsDto;
+
+public class GoodsDao extends DaoBase{
+
+//	商品一覧取得
+		public List<GoodsDto> getGoodsList(){
+		List<GoodsDto> goodsDtoList= new ArrayList<GoodsDto>();
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("SELECT * FROM goods");
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				GoodsDto goodsDto = goodsToDto(rs);
+				goodsDtoList.add(goodsDto);
+			}
+		}catch(Exception e) {
+			goodsDtoList = null;
+			e.printStackTrace();
+		}
+		super.dbClose();
+		return goodsDtoList;
+	}
+	
+//	検索結果の商品一覧取得
+	public List<GoodsDto> getGoodsList(String search){
+		List<GoodsDto> goodsDtoList= new ArrayList<GoodsDto>();
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("SELECT * FROM goods WHERE goods_name = Like '%?%';");
+			stmt.setString(1, search);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				GoodsDto goodsDto = goodsToDto(rs);
+				goodsDtoList.add(goodsDto);
+			}
+		}catch(Exception e) {
+			goodsDtoList = null;
+			e.printStackTrace();
+		}
+		super.dbClose();
+		return goodsDtoList;
+	}
+	
+//	商品取得（ひとつ）
+	public GoodsDto getGoods(int id) {
+		GoodsDto goodsDto = null;
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("SELECT * FROM goods WHERE id = ?;");
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				goodsDto = goodsToDto(rs);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		super.dbClose();
+		return goodsDto;
+	}
+	
+//	商品登録
+	public boolean insertGoods(GoodsDto goodsDto) {
+		boolean isSuccess = false;
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("INSERT INTO goods("
+					+ "goods_name,price,stock,description,image_dir,sales_quantity,sale,category_id,brand_id "
+					+ ")VALUES(?,?,?,?,?,?,?,?,?);");
+			stmt.setString(1, goodsDto.getGoodsName());
+			stmt.setInt(2, goodsDto.getPrice());
+			stmt.setInt(3, goodsDto.getStock());
+			stmt.setString(4, goodsDto.getDescription());
+			stmt.setString(5, goodsDto.getImageDir());
+			stmt.setInt(6, goodsDto.getSalesQuantity());
+			stmt.setBoolean(7, goodsDto.isSale());
+			stmt.setInt(8, goodsDto.getCategoryId());
+			stmt.setInt(9, goodsDto.getBrandId());
+			stmt.executeUpdate();
+			isSuccess = true;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		super.dbClose();
+		return isSuccess;
+	}
+
+//	商品削除
+	public boolean deleteGoods(int id){
+		boolean isSuccess = false;
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("DELETE FROM goods WHERE id = ?");
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+			isSuccess = true;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		super.dbClose();
+		return isSuccess;
+	}
+	
+//	商品更新
+	public boolean updateGoods(GoodsDto goodsDto) {
+		boolean isSuccess = false;
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("UPDATE SET goods_name=?,price=?,stock=?,description=?,"
+					+ "image_dir=?,sales_quantity=?,sale=?,category_id=?,brand_id=? WHERE id=?;");
+			stmt.setString(1, goodsDto.getGoodsName());
+			stmt.setInt(2, goodsDto.getPrice());
+			stmt.setInt(3, goodsDto.getStock());
+			stmt.setString(4, goodsDto.getDescription());
+			stmt.setString(5, goodsDto.getImageDir());
+			stmt.setInt(6, goodsDto.getSalesQuantity());
+			stmt.setBoolean(7, goodsDto.isSale());
+			stmt.setInt(8, goodsDto.getCategoryId());
+			stmt.setInt(9, goodsDto.getBrandId());
+			stmt.setInt(10, goodsDto.getId());
+			stmt.executeUpdate();
+			isSuccess = true;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		super.dbClose();
+		return isSuccess;
+	}
+	
+//	goodsから取得したカラムをDtoに詰め替え
+	private GoodsDto goodsToDto(ResultSet rs) {
+		GoodsDto goodsDto = new GoodsDto();
+		try {
+			goodsDto.setId(this.rs.getInt("id"));
+			goodsDto.setGoodsName(this.rs.getString("goods_name"));
+			goodsDto.setPrice(this.rs.getInt("price"));
+			goodsDto.setStock(this.rs.getInt("stock"));
+			goodsDto.setDescription(this.rs.getString("description"));
+			goodsDto.setImageDir(this.rs.getString("image_dir"));
+			goodsDto.setCategoryId(this.rs.getInt("category_id"));
+			goodsDto.setBrandId(this.rs.getInt("brand_id"));
+			goodsDto.setSale(this.rs.getBoolean("sale"));
+			goodsDto.setSalesQuantity(this.rs.getInt("sales_quantity"));
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return goodsDto;
+		
+	}
+}
