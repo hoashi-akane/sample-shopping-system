14bbf7f5921b316b707029ed8319fa7fd91a282e
oshita <1801151@s.asojuku.ac.jp>
2020-06-18 13:07:55 +0900

    追加：ログイン、ログアウト

---
+++ SampleShopping/src/servlet/LoginAdminServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public LoginAdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/jsp/login.jsp").forward(request, response);
+
+	}
+
+
+}
