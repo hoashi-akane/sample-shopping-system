593693a6a9b07139a3534796cf1a2d5445b45e4d
hoashi <akane.hoashi@outlook.jp>
2020-06-18 13:12:49 +0900

    追加：購入履歴系Dto

---
+++ SampleShopping/src/dto/BuyHistoryDetailDto.java
@@ -0,0 +1,48 @@
+package dto;
+
+public class BuyHistoryDetailDto {
+	
+	private int id;
+	private int buyHistoryId;
+	private int goodsId;
+	private int volume;
+	private int unitPrice;
+	private boolean isHidden;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBuyHistoryId() {
+		return buyHistoryId;
+	}
+	public void setBuyHistoryId(int buyHistoryId) {
+		this.buyHistoryId = buyHistoryId;
+	}
+	public int getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(int goodsId) {
+		this.goodsId = goodsId;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public boolean isHidden() {
+		return isHidden;
+	}
+	public void setHidden(boolean isHidden) {
+		this.isHidden = isHidden;
+	}
+}
+
---
+++ SampleShopping/src/dto/BuyHistoryDto.java
@@ -0,0 +1,30 @@
+package dto;
+
+import java.util.Date;
+
+public class BuyHistoryDto {
+	
+	private int id;
+	private int userId;
+	private Date buyDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public Date getBuyDate() {
+		return buyDate;
+	}
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+	
+}
