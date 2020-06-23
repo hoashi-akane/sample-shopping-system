1b2d4900f81a1f001b3a42345747f5b2ffff7ca4
hoashi <akane.hoashi@outlook.jp>
2020-06-23 20:08:44 +0900

    変更：BuyHistoryDtoにgoodsのリストを追加。

--- SampleShopping/src/dto/BuyHistoryDto.java
+++ SampleShopping/src/dto/BuyHistoryDto.java
@@ -1,11 +1,13 @@
 package dto;
 
 import java.util.Date;
+import java.util.List;
 
 public class BuyHistoryDto {
 	
 	private int id;
 	private int userId;
+	private List<GoodsDto> goodsDtoList;
 	private Date buyDate;
 	
 	public int getId() {
@@ -19,6 +21,12 @@ public class BuyHistoryDto {
 	}
 	public void setUserId(int userId) {
 		this.userId = userId;
+	}
+	public List<GoodsDto> getGoodsDtoList() {
+		return goodsDtoList;
+	}
+	public void setGoodsDtoList(List<GoodsDto> goodsDtoList) {
+		this.goodsDtoList = goodsDtoList;
 	}
 	public Date getBuyDate() {
 		return buyDate;
