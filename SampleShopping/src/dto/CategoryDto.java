c77a8d2551bfdf3be6e10fc15073832b2f72a318
hoashi <akane.hoashi@outlook.jp>
2020-06-26 14:15:13 +0900

    追加：#10 ブランド用Dto、カテゴリ用Dto

---
+++ SampleShopping/src/dto/CategoryDto.java
@@ -0,0 +1,31 @@
+package dto;
+
+public class CategoryDto {
+	private int id;
+	private String name;
+	
+	public CategoryDto(Integer id, String name) {
+		// goods用コンストラクタ
+		if(id == 0) {
+			this.id = 1;
+			this.name = "未指定";
+		}else {
+			this.id = id;
+			this.name = name;
+		}
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
