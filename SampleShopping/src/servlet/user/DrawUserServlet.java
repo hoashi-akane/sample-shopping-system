f4577aef8afa9dc52574984067e18c8fcc6e8f35
fujita <1801181@s.asojuku.ac.jp>
2020-06-30 10:35:27 +0900

    追加：#2 会員退会サーブレット

---
+++ SampleShopping/src/servlet/user/DrawUserServlet.java
@@ -0,0 +1,45 @@
+package servlet.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import dto.UserDto;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/drawuser")
+public class DrawUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DrawUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	HttpSession session = request.getSession();
+		UserDto userDto = (UserDto)session.getAttribute("useruto");
+		int id = userDto.getId();
+
+		UserDao userDao = new UserDao();
+		if(userDao.deleteUser(id)) {
+			response.sendRedirect("/SampleShopping/menu");
+		}else{
+			response.sendRedirect("/SampleShopping/dispuserinfo");
+		}
+	}
+}
