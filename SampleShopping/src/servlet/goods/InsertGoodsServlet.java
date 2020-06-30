f5821ac70af0d60ab234cc74624ecc3801640e58
oshita <1801151@s.asojuku.ac.jp>
2020-06-30 10:03:56 +0900

    追加：#5 商品追加、更新、削除

---
+++ SampleShopping/src/servlet/goods/InsertGoodsServlet.java
@@ -0,0 +1,62 @@
+package servlet.goods;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.GoodsDao;
+import dto.GoodsDto;
+
+/**
+ * Servlet implementation class InsertAbsenceResistServlet
+ */
+@WebServlet("/insertgoods")
+public class InsertGoodsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertGoodsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/jsp/insertgoods.jsp").
+		forward(request,response);
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+
+		HttpSession session = request.getSession();
+		GoodsDto goodsDto = new GoodsDto();
+
+	    goodsDto = (GoodsDto)session.getAttribute("goodsDto");
+
+		GoodsDao goodsDao = new GoodsDao();
+		boolean successGoods = goodsDao.insertGoods(goodsDto);
+
+
+		String path ="";
+		if(successGoods) {
+			session.setAttribute("goodsDto", goodsDto);
+			path = "menu";
+			response.sendRedirect(path);
+			session.removeAttribute("goodsDto");
+		} else {
+			path = "dispgoodslist";
+			response.sendRedirect(path);
+		}
+	}
+
+
+}
