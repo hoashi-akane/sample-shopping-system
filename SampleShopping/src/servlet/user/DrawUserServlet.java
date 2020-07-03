48f6f1df1b61f7f8bf92824fa32d60a37bece1fe
fujita <1801181@s.asojuku.ac.jp>
2020-07-03 12:22:10 +0900

    変更：#2 会員退会サーブレット

--- SampleShopping/src/servlet/user/DrawUserServlet.java
+++ SampleShopping/src/servlet/user/DrawUserServlet.java
@@ -32,7 +32,7 @@ public class DrawUserServlet extends HttpServlet {
 	 */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	HttpSession session = request.getSession();
-		UserDto userDto = (UserDto)session.getAttribute("userdto");
+		UserDto userDto = (UserDto)session.getAttribute("userDto");
 		int id = userDto.getId();
 
 		UserDao userDao = new UserDao();
