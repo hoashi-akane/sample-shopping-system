ee8aafd1127145fd7e1f98dd0fb447484d0b79b1
fujita <1801181@s.asojuku.ac.jp>
2020-07-03 12:15:57 +0900

    #3：情報変更サーブレット

--- SampleShopping/src/servlet/user/UpdateUserServlet.java
+++ SampleShopping/src/servlet/user/UpdateUserServlet.java
@@ -9,20 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.UserDao;
 import dto.UserDto;
-
 
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/dispuserinfo")
-public class DispUserInfoServlet extends HttpServlet {
+@WebServlet("/updateuser")
+public class UpdateUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DispUserInfoServlet() {
+    public UpdateUserServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,10 +31,15 @@ public class DispUserInfoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     	HttpSession session = request.getSession();
 		UserDto userDto = (UserDto)session.getAttribute("userDto");
-		request.setAttribute("userDto", userDto);
-		request.getRequestDispatcher("WEB-INF/jsp/createuser.jsp").forward(request, response);
+		UserDao userDao = new UserDao();
+
+		if(userDao.updateUser(userDto)) {
+			response.sendRedirect("/SampleShopping/menu");
+		}else{
+			response.sendRedirect("/SampleShopping/dispuserinfo");
+		}
 	}
 }
-
