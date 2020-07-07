76b90a55438de5e6735768893f9e9c8190f10d50
hoashi <akane.hoashi@outlook.jp>
2020-07-07 23:49:19 +0900

    追加 #29: Stripe用顧客idをDBで管理する為のメソッド

--- SampleShopping/src/dao/UserDao.java
+++ SampleShopping/src/dao/UserDao.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.sql.ResultSet;
+
 import dto.UserDto;
 
 public class UserDao extends UserDaoBase{
@@ -27,6 +29,45 @@ public class UserDao extends UserDaoBase{
 			isSuccess = true;
 		}catch(Exception e) {
 			e.printStackTrace();
+		}finally {
+			super.dbClose();
+		}
+		return isSuccess;
+	}
+
+//	カスタマーid取得
+	public String getPaymentCustomerId(int userId) {
+		String paymentCustomerId = null;
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("SELECT payment_customer_id FROM users WHERE id = ?");
+			stmt.setInt(1, userId);
+			ResultSet rs = stmt.executeQuery();
+			rs.next();
+			paymentCustomerId = rs.getString("payment_customer_id");
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			super.dbClose();
+		}
+		
+		return paymentCustomerId;
+	}
+	
+//	カスタマーidを更新（登録する)
+	public boolean updatePaymentCustomerId(int userId, String customerId) {
+		boolean isSuccess = false;
+		try {
+			con = super.dbOpen();
+			stmt = this.con.prepareStatement("UPDATE users SET payment_customer_id = ? WHERE id = ?");
+			stmt.setString(1, customerId);
+			stmt.setInt(2, userId);
+			stmt.executeUpdate();
+			isSuccess = true;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			super.dbClose();
 		}
 		return isSuccess;
 	}
