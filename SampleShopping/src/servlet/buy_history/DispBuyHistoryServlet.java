8871dabf33aadf68fa5fb5d0d6578565439974f9
hoashi <akane.hoashi@outlook.jp>
2020-07-13 17:43:49 +0900

    #56: パッケージを移動

--- SampleShopping/src/servlet/buy_history/DispBuyHistoryServlet.java
+++ SampleShopping/src/servlet/buy_history/DispBuyHistoryServlet.java
@@ -1,26 +1,31 @@
-package buy_history;
+package servlet.buy_history;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.BuyHistoryDao;
+import dto.BuyHistoryDto;
+import dto.UserDto;
 
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/hiddenbuyhistory")
-public class HiddenBuyHistoryServlet extends HttpServlet {
+@WebServlet("/dispbuyhistory")
+public class DispBuyHistoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HiddenBuyHistoryServlet() {
+    public DispBuyHistoryServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,13 +34,15 @@ public class HiddenBuyHistoryServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	int id = Integer.parseInt(request.getParameter("id"));
-    	BuyHistoryDao buyHistoryDao = new BuyHistoryDao();
 
-    	if(BuyHistoryDao.hiddenBuyHistory(id)) {
-			response.sendRedirect("/SampleShopping/menu");
-		}else{
-			response.sendRedirect("/SampleShopping/dispbuyhistory");
-		}
-    }
+    	List<BuyHistoryDto> listBuyHistory = new ArrayList<BuyHistoryDto>();
+    	BuyHistoryDao buyHistoryDao = new BuyHistoryDao();
+    	HttpSession session = request.getSession();
+		UserDto userDto = (UserDto)session.getAttribute("userDto");
+		int id = userDto.getId();
+
+		listBuyHistory = buyHistoryDao.getBuyHistoryList(id);
+		request.setAttribute("listBuyHistory", listBuyHistory);
+		request.getRequestDispatcher("WEB-INF/jsp/dispbuyhistory.jsp").forward(request, response);
+	}
 }
--- SampleShopping/src/servlet/buy_history/HiddenBuyHistoryServlet.java
+++ SampleShopping/src/servlet/buy_history/HiddenBuyHistoryServlet.java
@@ -1,31 +1,26 @@
-package buy_history;
+package servlet.buy_history;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.BuyHistoryDao;
-import dto.BuyHistoryDto;
-import dto.UserDto;
 
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/dispbuyhistory")
-public class DispBuyHistoryServlet extends HttpServlet {
+@WebServlet("/hiddenbuyhistory")
+public class HiddenBuyHistoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DispBuyHistoryServlet() {
+    public HiddenBuyHistoryServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,15 +29,13 @@ public class DispBuyHistoryServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    	List<BuyHistoryDto> listBuyHistory = new ArrayList<BuyHistoryDto>();
+    	int id = Integer.parseInt(request.getParameter("id"));
     	BuyHistoryDao buyHistoryDao = new BuyHistoryDao();
-    	HttpSession session = request.getSession();
-		UserDto userDto = (UserDto)session.getAttribute("userDto");
-		int id = userDto.getId();
 
-		listBuyHistory = buyHistoryDao.getBuyHistoryList(id);
-		request.setAttribute("listBuyHistory", listBuyHistory);
-		request.getRequestDispatcher("WEB-INF/jsp/dispbuyhistory.jsp").forward(request, response);
-	}
+    	if(BuyHistoryDao.hiddenBuyHistory(id)) {
+			response.sendRedirect("/SampleShopping/menu");
+		}else{
+			response.sendRedirect("/SampleShopping/dispbuyhistory");
+		}
+    }
 }
