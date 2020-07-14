54210078b4da653b736d211fdb7b3f2747dc5b39
oshita <1801151@s.asojuku.ac.jp>
2020-07-14 12:20:24 +0900

    変更： #5 getで送る情報の追加

--- SampleShopping/src/servlet/goods/UpdateGoodsServlet.java
+++ SampleShopping/src/servlet/goods/UpdateGoodsServlet.java
@@ -27,9 +27,41 @@ public class UpdateGoodsServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/jsp/updategoods.jsp").
+
+	try	{
+
+		HttpSession session = request.getSession();
+		GoodsDto goodsDto = new GoodsDto();
+	    goodsDto = (GoodsDto)session.getAttribute("goodsDto");
+
+	    String num1 = request.getParameter("goods_id");
+		int goods_id  = Integer.parseInt(num1);
+		String goods_name = request.getParameter("goods_name");
+		String num2 = request.getParameter("price");
+		int price  = Integer.parseInt(num2);
+		String num3 = request.getParameter("stock");
+		int stock  = Integer.parseInt(num3);
+		String description = request.getParameter("description");
+		String num4 = request.getParameter("category_id");
+		int category_id  = Integer.parseInt(num4);
+		String num5 = request.getParameter("brand_id");
+		int brand_id  = Integer.parseInt(num5);
+
+
+		goodsDto.setId(goods_id);
+		goodsDto.setGoodsName(goods_name);
+		goodsDto.setPrice(price);
+		goodsDto.setStock(stock);
+		goodsDto.setDescription(description);
+		goodsDto.setCategoryId(category_id);
+		goodsDto.setBrandId(brand_id);
+
+		request.getRequestDispatcher("/WEB-INF/jsp/correctiongoods.jsp").
 		forward(request,response);
+
+		}catch(Exception e) {
 	}
+}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
