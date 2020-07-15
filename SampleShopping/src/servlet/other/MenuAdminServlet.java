0afdcb0341f68d9ca85cf41af1e197ee9b387fd9
ogawa-mayuka <1801188oga@gmail.com>
2020-07-15 14:01:58 +0900

    追加：#73 管理者側のメニューサーブレット

---
+++ SampleShopping/src/servlet/other/MenuAdminServlet.java
@@ -0,0 +1,39 @@
+package servlet.other;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MenuAdminServlet
+ */
+@WebServlet("/MenuAdminServlet")
+public class MenuAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MenuAdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+
+		request.getRequestDispatcher("/WEB-INF/jsp/topadmin.jsp").forward(request,response);
+
+	}
+
+}
+
