bb3585c823771354824ab826b20851cac610b1ee
ogawa-mayuka <1801188oga@gmail.com>
2020-07-15 14:28:39 +0900

    変更：#73 管理者側のメニューサーブレット

--- SampleShopping/src/servlet/other/MenuAdminServlet.java
+++ SampleShopping/src/servlet/other/MenuAdminServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class MenuAdminServlet
  */
-@WebServlet("/MenuAdminServlet")
+@WebServlet("/menuadmin")
 public class MenuAdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
